c4cb0849502162b3d53a5a4b61a58b0dfda078df
AlexisGlawitsch <lexiglawitsch@gmail.com>
2021-04-09 21:14:01 -0700

    Started landing page + background color selection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,45 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [background, setBackground] = useState('color-default');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to [Chatbot Name]!</p>
-      </header>
+    <div className={`App ${background}`}>
+      <Chatbox onColorClick={(color) => setBackground(color)}/>
     </div>
   );
+}
+
+function Chatbox(props) {
+  return (
+    <div className="chatbox">
+        <div className="p-squished">
+          <p>Welcome to [Chatbot Name]!</p>
+          <p>You can talk to me about anything related to mental health.</p>
+        </div>
+        <p>To begin, please select a color:</p>
+        <div className="color-list">
+          <CircleButton color="color-red" onClick={props.onColorClick}></CircleButton>
+          <CircleButton color="color-orange" onClick={props.onColorClick}></CircleButton>
+          <CircleButton color="color-yellow" onClick={props.onColorClick}></CircleButton>
+          <CircleButton color="color-green" onClick={props.onColorClick}></CircleButton>
+          <CircleButton color="color-blue" onClick={props.onColorClick}></CircleButton>
+          <CircleButton color="color-purple" onClick={props.onColorClick}></CircleButton>
+          <CircleButton color="color-black" onClick={props.onColorClick}></CircleButton>
+        </div>
+      </div>
+  )
+}
+
+function CircleButton(props) {
+  return (
+    <button 
+      className={`circle-button ${props.color}`}
+      onClick={() => props.onClick(props.color)}
+    >
+    </button>
+  )
 }
 
 export default App;
