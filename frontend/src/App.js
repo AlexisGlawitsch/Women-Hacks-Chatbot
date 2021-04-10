9c90a98ef8253f1693df61f4fac8273ea4ab4e90
AlexisGlawitsch <lexiglawitsch@gmail.com>
2021-04-09 20:12:51 -0700

    Restructured directory and started frontend theming

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Chatbot</p>
+        <p>Welcome to [Chatbot Name]!</p>
       </header>
     </div>
   );
